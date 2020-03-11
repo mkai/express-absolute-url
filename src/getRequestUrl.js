68777c159ed92f757bff7c56205b2dd42b5112c9
Markus Kaiserswerth <github@sensun.org>
2020-03-11 21:21:59 +0100

    Make naming more clear

--- src/getRequestUrl.js
+++ src/getRequestUrl.js
@@ -4,20 +4,21 @@ function getAuthString(username, password) {
   return [username, password].filter(Boolean).join(':');
 }
 
-function getBaseUrl({ protocol, username, password, hostname, app }) {
-  const [parsedHostname, parsedPort] = hostname.split(':');
+function getBaseUrlString({ protocol, username, password, hostname, app }) {
+  // In Express 5, req.hostname might contain the port – remove it if needed.
+  const [bareHostname] = hostname.split(':');
 
   return url.format({
     protocol,
     auth: getAuthString(username, password),
-    hostname: parsedHostname,
     port: parsedPort || app.settings.port,
+    hostname: bareHostname,
   });
 }
 
-  const baseUrl = getBaseUrl(request);
 export function getRequestUrl(req) {
   const path = req.url;
+  const baseUrl = getBaseUrlString(req);
 
   return new URL(path, baseUrl);
 }
